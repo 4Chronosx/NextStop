d35e5a7527ebe8c4b285b071d38e433f64cfaaaa
4Chronosx <christianjamezz5@gmail.com>
2024-11-20 22:26:53 +0800

    added chatbox ai with gpt

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home/page';
+import CreateAi from './components/create-ai/page';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/create-with-ai" element={<CreateAi />}></Route>
       </Routes>
     </Router>
 
