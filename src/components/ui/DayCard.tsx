190c768866e2cecd92ee6625214a1b92371b2d76
4Chronosx <christianjamezz5@gmail.com>
2024-12-08 20:10:14 +0800

    finsished view page and create ai

---
+++ src/components/ui/DayCard.tsx
@@ -0,0 +1,52 @@
+import "./DayCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import DialogBox from "./DialogBox";
+
+
+interface ActivityDetail {
+  "Activity Title": string;
+  "Activity Type": string;
+  Duration: string;
+  "Time slot": string;
+  "Budget for the Activity": string;
+  Location: string;
+}
+
+interface DayItinerary {
+  date: string;
+  details: ActivityDetail[];
+}
+
+interface DayItineraryProp {
+  dayItinerary: DayItinerary;
+}
+
+interface Prop {
+  dayNum: number;
+}
+
+
+const DayCard = ({ dayItinerary, dayNum } : DayItineraryProp&Prop) => {
+  return (
+    <div className="dayCard-container">
+      <div className="dayCard-left">
+        <div className="icon">
+          <FontAwesomeIcon icon={faCalendar as any} size="3x" />
+        </div>
+        <div className="text-container">
+          <p className="day-title">Day {dayNum}</p>
+          <p className="preview-detail">{dayItinerary.details.length} activities</p>
+        </div>
+      </div>
+      <div className="dayCard-right">
+        <div className="dayCard-button-container">
+          <DialogBox date={dayItinerary.date} details={dayItinerary.details}></DialogBox>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default DayCard;
---
+++ src/components/ui/DialogBox.tsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import Dialog from "@mui/material/Dialog";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import ActivityTimeLine from "./ActivityTimeLine";
+import './DialogBox.css'
+
+
+
+interface ActivityDetail {
+    "Activity Title": string;
+    "Activity Type": string;
+    Duration: string;
+    "Time slot": string;
+    "Budget for the Activity": string;
+    Location: string;
+  }
+  
+  interface DayItinerary {
+    date: string;
+    details: ActivityDetail[];
+  }
+  
+
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<unknown>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const DialogBox = ({ details, date } : DayItinerary) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  console.log(date);
+
+  return (
+    <React.Fragment>
+      <button className="dialogView-button" onClick={handleClickOpen}>view</button>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Activities of the Day {date}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <section className="dialog-content-container">
+          <div className="timeline-container">
+            <ActivityTimeLine date={date} details={details}></ActivityTimeLine>
+          </div>
+          <div className="maps-container">
+            Here is the maps section with its corresponding routes
+          </div>
+        </section>
+      </Dialog>
+    </React.Fragment>
+  );
+};
+
+export default DialogBox;
